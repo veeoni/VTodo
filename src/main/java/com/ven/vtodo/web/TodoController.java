45b3d834b4f2b23813ef9895bf43727c0f579ef4
VEEONI <514754220@qq.com>
2021-04-09 20:22:22 +0800

    design the ui of add new todo

--- src/main/java/com/ven/vtodo/web/TodoController.java
+++ src/main/java/com/ven/vtodo/web/TodoController.java
@@ -12,7 +12,9 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -27,18 +29,24 @@ public class TodoController {
     private TodoService todoService;
 
     @GetMapping("/")
-    public String todo(
-            @PageableDefault(size = 8, sort = {"updateTime"}, direction = Sort.Direction.DESC) Pageable pageable,
-            Model model){
+    public String todo(Model model){
         //model用于与前端Thymeleaf传递数据，键值对
-        model.addAttribute("page", blogService.listBlog(pageable));
         model.addAttribute("types", typeService.listTypeTop(6));//可定义在配置文件
         model.addAttribute("tags", tagService.listTagTop(10));
         model.addAttribute("recommendBlogs", blogService.listRecommendBlogTop(8));
-        List<Todo> todos = todoService.listTodo();
-
+//        List<Todo> todos = todoService.listTodo();
+        //TODO 分类所有todos
         model.addAttribute("todos", todoService.listTodo());
         System.out.println("----------index--------------");
         return "todo";
     }
+
+    // TODO: 2021/4/9 ajax刷新todo的那两个segment
+    @GetMapping("/todos/{date}")
+    private String todoByDate(@PathVariable Date date, Model model){
+        
+        return "";
+    }
+
+    // TODO: 2021/4/9 添加 
 }
